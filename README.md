063c912f4ebfacbfad034625a49b81d72dbd1d73
MathisGredtYnov <mathis.gredt@ynov.com>
2023-03-16 19:08:18 +0100

    requete pseudo correct

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # GroupieTracker_Gredt_Mathis
+
+## Ce projet à été réalisé dans le cadre d'un exercice au sein du campus Ynov pour nous apprendre l'utilisation des API.  
+Pour cela, le projet était de réaliser un site web permettant d'afficher les requêtes d'un API préalablement sélectionné.  
+
+Voici ci-dessous une documentation de l'organisation de mon travail pour se projet:  
+  
+## Organisation:  
+  
+- Tout d'abord j'ai créer un document word pour expliquer l'avancement de mon travail pour chaque jour. Celui si contient toute la progression de ce projet.  
+
+## Arboresence:
+  
+### GO
+
+- Le dossier Go contient le fichier main.go qui permet de lancer les templates html, de charger les css ainsi que les images et tout les assets nécessaire.  
+C'est aussi lui qui permet de récupérer toute les informations de l'API et de les stocker pour pouvoir ensuite les afficher en WEB.
+
+
+### WEB  
+  
+- Le dossier Web contient 2 sous dossier, ASSETS qui regroupe tout les assets utilisé pour le visuel du site (logo, font, image).  
+Le deuxième sous dossier se nomme "css" et contient tout les fichier stylistique du site.  
+  
+- Enfin il reste les fichiers HTML qui sont les structures du site et qui permettent d'afficher les données de l'API qui ont été récupéré dans le GO.  
+
+
+
+
+
+
+
---
+++ main.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+type PlayerInformation struct {
+	Avatar     string `json:"avatar"`
+	PlayerID   int    `json:"player_id"`
+	ID         string `json:"@id"`
+	URL        string `json:"url"`
+	Name       string `json:"name"`
+	Username   string `json:"username"`
+	Title      string `json:"title"`
+	Followers  int    `json:"followers"`
+	Country    string `json:"country"`
+	Location   string `json:"location"`
+	LastOnline int    `json:"last_online"`
+	Joined     int    `json:"joined"`
+	Status     string `json:"status"`
+	IsStreamer bool   `json:"is_streamer"`
+	Verified   bool   `json:"verified"`
+}
+
+type PlayerData struct {
+	Success   bool
+	Entree    string
+	Data      *PlayerInformation
+	DataTitle *PlayerTitle
+}
+
+type PlayerTitle struct {
+	Players []string `json:"players"`
+}
+
+func main() {
+	var p PlayerData
+	p.init()
+	p.InitTitle()
+	//démarage serveur
+	fmt.Println("Server is running on port 80 http://localhost")
+	//gestion css
+	fs := http.FileServer(http.Dir("WEB/css"))
+	http.Handle("/css/", http.StripPrefix("/css/", fs))
+	//gestion assets
+	image := http.FileServer(http.Dir("WEB/ASSETS"))
+	http.Handle("/ASSETS/", http.StripPrefix("/ASSETS/", image))
+	//gestion html
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/requete", p.requeteHandler)
+	http.ListenAndServe(":80", nil)
+}
+
+func (p *PlayerData) InitTitle() {
+	url := "https://api.chess.com/pub/titled/GM"
+
+	// Envoyer une requête GET à l'API
+	res, err := http.Get(url)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer res.Body.Close()
+
+	// Parser la réponse JSON
+	var playerInfo PlayerTitle
+	err = json.NewDecoder(res.Body).Decode(&playerInfo)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func (p *PlayerData) RechercheTitle(title string) *PlayerTitle {
+	url := "https://api.chess.com/pub/titled/" + title
+	res, err := http.Get(url)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer res.Body.Close()
+
+	// Parser la réponse JSON
+	var playerInfo PlayerTitle
+	err = json.NewDecoder(res.Body).Decode(&playerInfo)
+	if err != nil {
+		panic(err.Error())
+	}
+	return &playerInfo
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl1 := template.Must(template.ParseFiles("WEB/index.html"))
+	if r.Method != http.MethodPost {
+		tmpl1.Execute(w, nil)
+	}
+}
+
+func (p *PlayerData) requeteHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("WEB/requete.html"))
+	details := PlayerData{
+		Success:   true,
+		Entree:    r.PostFormValue("title"),
+		Data:      p.recherche(r.FormValue("search")),
+		DataTitle: p.RechercheTitle(r.PostFormValue("title")),
+	}
+	tmpl.Execute(w, details)
+}
+
+// Fonction qui récupère le profile d'un joueur
+func (p *PlayerData) init() {
+
+	url := "https://api.chess.com/pub/player/erik"
+
+	// Envoyer une requête GET à l'API
+	res, err := http.Get(url)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer res.Body.Close()
+
+	// Parser la réponse JSON
+	var playerInfo PlayerInformation
+	err = json.NewDecoder(res.Body).Decode(&playerInfo)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func (p *PlayerData) recherche(pseudo string) *PlayerInformation {
+	url := "https://api.chess.com/pub/player/" + pseudo
+
+	// Envoyer une requête GET à l'API
+	res, err := http.Get(url)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer res.Body.Close()
+
+	// Parser la réponse JSON
+	var playerInfo *PlayerInformation
+	err = json.NewDecoder(res.Body).Decode(&playerInfo)
+	if err != nil {
+		panic(err.Error())
+	}
+	if playerInfo.Avatar == "" {
+		playerInfo.Avatar = "ASSETS/NoAvatar.gif"
+	}
+
+	return playerInfo
+
+}
